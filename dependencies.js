dfa1d47b75a6bbe7e41db0031bccc3ea099d1f1d
mark vicuna <markcvicuna@gmail.com>
2016-05-03 15:44:37 -0400

    initial commit

---
+++ dependencies.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+module.exports = function(wagner,app) {
+  wagner.factory('Config', function() {
+    configFile = app.get('env') === 'development'
+      ? './config/dev/config.json'
+      : './config/prod/config.json';
+    return JSON.parse(fs.readFileSync(configFile).toString());
+  });
+};
