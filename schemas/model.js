d5636051f4aa28737823d1cad6060dff5aec0e8b
mark vicuna <markcvicuna@gmail.com>
2016-05-04 01:31:24 -0400

    untested login and user creation

--- schemas/model.js
+++ schemas/model.js
@@ -1,31 +1,23 @@
 var mongoose = require('mongoose');
-var _ = require('underscore');
 
 module.exports = function(wagner) {
-  mongoose.connect(wagner.get(dbConnectionString));
+  var dbConnectionString = wagner.invoke(function(Config) {
+    return Config.dbConnectionString;
+  });
+  mongoose.connect(dbConnectionString);
 
   wagner.factory('db', function() {
     return mongoose;
   });
 
-  var Category =
-    mongoose.model('Category', require('./category'), 'categories');
-  var User =
-    mongoose.model('User', require('./user'), 'users');
-
   var models = {
-    Category: Category,
-    User: User
+    User: mongoose.model('User', require('./user'), 'users')
   };
 
-  // To ensure DRY-ness, register factories in a loop
-  _.each(models, function(value, key) {
-    wagner.factory(key, function() {
-      return value;
-    });
-  });
 
-  wagner.factory('Product', require('./product'));
+  wagner.factory('Model', function() {
+      return models;
+  });
 
   return models;
 };
---
+++ schemas/user.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+module.exports = new mongoose.Schema({
+  profile: {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true
+    },
+    picture: {
+      type: String,
+      required: true
+    }
+  },
+  data: {
+    oauth: {
+      type: String,
+      required: true }
+  },
+  createdOn: { type: Date, required: true },
+  logOn: { type: Date, required: true },
+});
+
+module.exports.set('toObject', { virtuals: true });
+module.exports.set('toJSON', { virtuals: true });
